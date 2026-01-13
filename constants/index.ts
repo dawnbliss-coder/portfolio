4db052591d4db59ab56233075073b3ebe75758e5
Priyanka Agarwal <agarwalpriyanka42875@gmail.com>
2026-01-13 18:21:27 +0530

    Include constants folder for deployment

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const projects = [
+    {title : "Quantitative Loan Risk Analysis",
+        description : "Completed a JPMorgan Chase & Co. Quantitative Research Virtual Experience via Forage (Dec 2025). Developed a probability-of-default model and utilized dynamic programming to optimize FICO score categorization for financial risk assessment.",
+        tags : ["Python", "Pandas", "NumPy", "Scikit-learn"],
+        link : "https://github.com/dawnbliss-coder/quantitative-research-projects",
+        image : "/project1.png",
+    },
+];
+
+export const skills = [
+    "C","C++","Python","JavaScript","SQL","Operating Systems", "Distributed Systems", "NumPy", "Pandas", "Matplotlib", "Scikit-learn", "Flask", "MongoDB"
+];
+
+export const ACHIEVEMENTS = [
+    {
+      title: "JEE Main 2024",
+      detail: "All India Rank 2032 (Top 0.2% of 1.2M candidates)"
+    },
+    {
+      title: "JEE Advanced 2024",
+      detail: "All India Rank 5130"
+    },
+    {
+      title: "Dean's List - IIIT Hyderabad",
+      detail: "Awarded for Academic Excellence (Monsoon 2024)"
+    }
+  ];
